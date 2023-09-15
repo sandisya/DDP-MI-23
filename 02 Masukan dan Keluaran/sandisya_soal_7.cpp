4b043ab66186745124d28a006b4b2d5acc3a1b39
sandisya <sandisyaputra1104@gmail.com>
2023-09-15 07:48:13 +0700

    Tugas 2 DDP

--- 02 Masukan dan Keluaran/sandisya_soal_7.cpp
+++ 02 Masukan dan Keluaran/sandisya_soal_7.cpp
@@ -5,16 +5,16 @@ using namespace std;
 int main()
 {
 
-    string x[12];
+    string nama[12];
 
-    cin >> x[0] >> x[1] >> x[2];
-    cin >> x[3] >> x[4] >> x[5];
-    cin >> x[6] >> x[7] >> x[8];
-    cin >> x[9] >> x[10] >> x[11];
+    cin >> nama[0] >> nama[1] >> nama[2];
+    cin >> nama[3] >> nama[4] >> nama[5];
+    cin >> nama[6] >> nama[7] >> nama[8];
+    cin >> nama[9] >> nama[10] >> nama[11];
 
-    cout << x[0] << " " << x[3] << " " << x[6] << " " << x[9] << endl;
-    cout << x[1] << " " << x[4] << " " << x[7] << " " << x[10] << endl;
-    cout << x[2] << " " << x[5] << " " << x[8] << " " << x[11] << endl;
+    cout << nama[0] << " " << nama[3] << " " << nama[6] << " " << nama[9] << endl;
+    cout << nama[1] << " " << nama[4] << " " << nama[7] << " " << nama[10] << endl;
+    cout << nama[2] << " " << nama[5] << " " << nama[8] << " " << nama[11] << endl;
 
     return 0;
 }
