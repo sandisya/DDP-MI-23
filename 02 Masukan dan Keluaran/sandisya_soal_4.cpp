b5dd31a94ca35845eeb177a18269b2b2c4d146ad
sandisya <sandisyaputra1104@gmail.com>
2023-09-22 05:48:19 +0700

    Tugas 3 DDP

--- 02 Masukan dan Keluaran/sandisya_soal_4.cpp
+++ 02 Masukan dan Keluaran/sandisya_soal_4.cpp
@@ -6,9 +6,10 @@ int main()
 {
     float value1, value2;
     cin >> value1 >> value2;
-    cout << value1 + value2 << endl;
-    cout << value1 - value2 << endl;
-    cout << value1 * value2 << endl;
-    cout << value1 / value2;
+
+    cout << "Hasil dari Penjumlahan " << value1 << " dengan " << value2 << " adalah " << value1 + value2 << endl;
+    cout << "Hasil dari Pengurangan " << value1 << " dengan " << value2 << " adalah " << value1 - value2 << endl;
+    cout << "Hasil dari Perkalian " << value1 << " dengan " << value2 << " adalah " << value1 * value2 << endl;
+    cout << "Hasil dari Pembagian " << value1 << " dengan " << value2 << " adalah " << value1 / value2 << endl;
     return 0;
 }
