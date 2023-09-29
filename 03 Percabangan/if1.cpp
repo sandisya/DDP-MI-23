d4b1191e64d88f3d07731fe84004650e3a090e38
sandisya <sandisyaputra1104@gmail.com>
2023-09-29 08:48:39 +0700

    Percabangan if

---
+++ 03 Percabangan/if1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int nilai;
+    cin >> nilai;
+    if (nilai == 50)
+    {
+        cout << "SETENGAH";
+    }
+}
---
+++ 03 Percabangan/if2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int nilai;
+    cin >> nilai;
+    if (nilai == 100)
+    {
+        cout << "SEMPURNA";
+    }
+    else
+    {
+        cout << "BELUM SEMPURNA";
+    }
+}
