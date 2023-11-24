1edb0c35bf9623dcb0b83bb5dd7e3c5e9d2e2c80
Sandi Syaputra <sandisyaputra1104@gmail.com>
2023-11-24 08:55:04 +0700

    Searching

---
+++ 08 Searching/binari.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n, cari;
+    bool ketemu = false;
+
+    //menentukan panjang array
+    cout << "Input jumlah data:  ";
+    cin >> n;
+    cout << endl;
+
+    //memasukan nilai ke dalam array
+    int list_data[n];
+    for (int i = 1; i <= n; i++){
+        cout << "Input data ke-" << i << ": ";
+        cin >> list_data[i];
+    }
+    cout << endl;
+
+    //menentukan nilai yang ingin dicari
+    cout << "Input bilangan yang dicari: ";
+    cin >> cari;
+    cout << endl;
+
+    //mencari dengan sequential search
+    for(int i = 0; i <= n; i++){
+        if(list_data[i] == cari){
+            ketemu = true;
+            cout << "Data ditemukan pada index ke-"<<i;
+        }
+    }
+    if(!ketemu)
+    cout << "Data tidak ditemukan";
+}
